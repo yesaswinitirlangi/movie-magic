6467836ff5d816594d1936a209431c2233bd1f0c
yesaswinitirlangi <tirlangiyesaswini@gmail.com>
2025-07-05 11:48:53 +0530

    Updated app.py with latest changes

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def book_ticket():
 # Run the application
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
